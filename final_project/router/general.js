b8848f597e7f2c845b55d7aed0fdae970476846e
Chuck Alexander <44351873+seegeealex@users.noreply.github.com>
2026-02-02 18:05:38 -0500

    Redo Tasks 10-13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios').default;
 
 public_users.post("/register", (req,res) => {
   const username = req.body.username;
@@ -15,13 +16,24 @@ public_users.post("/register", (req,res) => {
     return res.status(404).send(`User ${username} is already registered.`);
   }
   users[username] = password;
-  return res.status(200).send(`User ${username} has been registered.`);
+  console.log(username, password);
+  return res.status(200).send(`User ${username} successfully registered.`);
 
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   return res.status(200).send(JSON.stringify(books,null,4));
+});
+
+// The same, but with async/await & axios
+public_users.get('/async', async function(req, res) {
+  try {
+    const outcome = await axios.get('http://localhost:5000');
+    return res.status(200).send(outcome.data);
+  } catch (err) {
+    return res.status(400).send(err.toString());
+  }
 });
 
 // Get book details based on ISBN
@@ -31,10 +43,21 @@ public_users.get('/isbn/:isbn',function (req, res) {
   if (match) {
     return res.status(200).send(JSON.stringify(match, null, 4));
   } else {
-    return res.status(200).send(`ISBN ${isbn} not found.`);
+    return res.status(400).send(`ISBN ${isbn} not found.`);
   }
  });
-  
+
+// Same, but with async/await & axios
+public_users.get('/async/isbn/:isbn', async function(req, res) {
+  const isbn = req.params.isbn;
+  try {
+    const outcome = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+    return res.status(200).send(outcome.data);
+  } catch (err) {
+    return res.status(400).send(err.toString());
+  }
+});
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   const isbns = Object.keys(books);
@@ -51,6 +74,17 @@ public_users.get('/author/:author',function (req, res) {
     return res.status(200).send(JSON.stringify(authorBooks, null, 4));
   } else {
     return res.status(200).send(`No books found by author '${author}'.`);
+  }
+});
+
+// Same, but with async/await & axios
+public_users.get('/async/author/:author', async function(req, res) {
+  const author = req.params.author;
+  try {
+    const outcome = await axios.get(`http://localhost:5000/author/${author}`);
+    return res.status(200).send(outcome.data);
+  } catch (err) {
+    return res.status(400).send(err.toString());
   }
 });
 
@@ -72,6 +106,18 @@ public_users.get('/title/:title',function (req, res) {
     return res.status(200).send(`No books found with title '${title}'.`);
   }
 });
+
+// Same, but with async/await & axios
+public_users.get('/async/title/:title', async function(req, res) {
+  const title = req.params.title;
+  try {
+    const outcome = await axios.get(`http://localhost:5000/title/${title}`);
+    return res.status(200).send(outcome.data);
+  } catch (err) {
+    return res.status(400).send(err.toString());
+  }
+});
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
